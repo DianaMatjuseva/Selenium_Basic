281217edba3a8475e0ae6c4b239d2aa93b40a1cc
Diana Matjuseva <ptr679@gmail.com>
2021-10-19 10:50:55 +0300

    Sample5Task

--- src/selenium/sample/Sample5Task.java
+++ src/selenium/sample/Sample5Task.java
@@ -36,21 +36,33 @@ public class Sample5Task {
     @Test
     public void goToAlertedPageViaButton() throws Exception {
 //         TODO:
-//        click on "To go to alerted page press Ok. Or stay here" button
-//        switch to alert
-//        click ok
-//        switch to second alert
-//        verify alert text
-//        click ok on second alert
-//        verify that the correct page is opened
+        //        click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
+        //        switch to alert
+        Alert firstAlert = driver.switchTo().alert();
+        //        click ok
+        firstAlert.accept();
+        //        switch to second alert
+        Alert secondAlert = driver.switchTo().alert();
+        //        verify alert text
+        assertEquals("Booooooooo!", secondAlert.getText());
+        //        click ok on second alert
+        secondAlert.accept();
+        //        verify that the correct page is opened
+        assertEquals("This page is alerted", driver.findElement(By.id("heading")).getText());
     }
+
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
 //         TODO:
-//        click on "To go to alerted page press Ok. Or stay here" button
-//        switch to alert
-//        click cancel
-//        verify the text on page
+        //        click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
+        //        switch to alert
+        Alert alert = driver.switchTo().alert();
+        //        click cancel
+        alert.dismiss();
+        //        verify the text on page
+        assertEquals("So you decided to say? Good!", driver.findElement(By.id("textForAlerts")).getText());
     }
 }
